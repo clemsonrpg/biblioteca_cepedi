4bd34042606711eb6a571306e5880fe19af87bad
clemsonrpg <loureiro.ravi@gmail.com>
2025-12-11 19:59:59 -0300

    first commit

--- apps/livros/views.py
+++ apps/livros/views.py
@@ -1,5 +1,7 @@
-from django.contrib import messages
+import time
 
+from django.contrib import messages
+from time import sleep
 from .forms import LivroForm
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Livro
@@ -20,4 +22,25 @@ def listar_livros(request):
     template_name = 'livros/listar_livros.html'
     livros = Livro.objects.all()
     context = {'relacao_livros': livros}
+    return render(request, template_name, context)
+
+def editar_livro(request, id):
+    template_name = 'livros/form_livro.html'
+    livro = get_object_or_404(Livro, id=id)
+    form = LivroForm(request.POST or None, request.FILES or None, instance=livro)
+    context = {'form': form}
+    if form.is_valid():
+        form.save()
+        messages.success(request, "Os dados foram atualizados com sucesso!")
+        return redirect('livros:listar_livros')
+    return render(request, template_name, context)
+
+def excluir_livro(request, id):
+    template_name = 'livros/excluir_livro.html'
+    livro = Livro.objects.get(id=id)
+    context = {'livro': livro}
+    if request.method == "POST":
+        livro.delete()
+        messages.error(request, 'O livro foi excluído com sucesso.')
+        return redirect('livros:listar_livros')
     return render(request, template_name, context)
